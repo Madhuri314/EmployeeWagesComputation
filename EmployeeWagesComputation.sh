484e6818eeb42cdd0d18a45d70c6ff6282b4bca3
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-13 00:29:29 +0530

    Calculated Part Time Employee Wages

--- EmployeeWagesComputation.sh
+++ EmployeeWagesComputation.sh
@@ -1,13 +1,16 @@
-#!/bin/bash -x
+#! /bin/bash -x
 
-isPresent=1
-randomCheck=$(( RANDOM%2 ))
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$(( RANDOM%3 ))
 
-if [ $isPresent -eq $randomCheck ]
+if [ $isFullTime -eq $randomCheck ]
 then
-        empHours=8
-        ratePerHour=20
-        empWagesPerDay=$(( $ratePerHour*$empHours ))
+        empHrs=8;
 else
-        empWagesPerDay=0
+        empHrs=0;
 fi
+
+salary=$(( $empHrs * $empRatePerHr ))
+echo $salary
